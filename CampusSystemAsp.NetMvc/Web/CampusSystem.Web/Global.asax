51aa5ea8b46c2b940a99578cdeb60fa43eb41386
vlad-sp <vladimirsopotlyanov@gmail.com>
2016-02-12 18:30:01 +0200

    Added Campus system solution with empty template.

---
+++ CampusSystemAsp.NetMvc/Web/CampusSystem.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CampusSystem.Web.MvcApplication" Language="C#" %>
---
+++ CampusSystemAsp.NetMvc/Web/CampusSystem.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CampusSystem.Web.Startup))]
+namespace CampusSystem.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
