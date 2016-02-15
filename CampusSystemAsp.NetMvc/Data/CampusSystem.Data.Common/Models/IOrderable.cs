bae77924799af6746a9559ce5eef15a41fd7eb91
vlad-sp <vladimirsopotlyanov@gmail.com>
2016-02-15 10:24:27 +0200

    Added repository pattern

---
+++ CampusSystemAsp.NetMvc/Data/CampusSystem.Data.Common/Models/IOrderable.cs
@@ -0,0 +1,10 @@
+﻿namespace CampusSystem.Data.Common.Models
+{
+    using System.ComponentModel.DataAnnotations.Schema;
+
+    public interface IOrderable
+    {
+        [Index]
+        int OrderBy { get; set; }
+    }
+}
