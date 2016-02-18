7275df46cf59a03e49d654cb683917174025fbf0
vlad-sp <vladimirsopotlyanov@gmail.com>
2016-02-18 10:54:41 +0200

    Added BaseController

---
+++ CampusSystemAsp.NetMvc/Web/CampusSystem.Web/Controllers/BaseController.cs
@@ -0,0 +1,11 @@
+﻿namespace CampusSystem.Web.Controllers
+{
+    using System.Web.Mvc;
+
+    using Services.Web;
+
+    public abstract class BaseController : Controller
+    {
+        public ICacheService Cache { get; set; }
+    }
+}
